9548ad5760a97661d0da0f31b4a9ccedc3f4f7f5
Nicolò Tagliaferro <nico.taglia26@gmail.com>
2019-05-06 09:57:27 +0200

    Aggiunto get/drivers
    #TODO calcolare l'età del pilota

--- InitializrSpringbootProject/src/main/java/it/prepattag/F1/GetController.java
+++ InitializrSpringbootProject/src/main/java/it/prepattag/F1/GetController.java
@@ -1,5 +1,7 @@
 package it.prepattag.F1;
 
+import java.sql.Date;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +16,27 @@ public class GetController {
 	public Map driver(@RequestParam("id") int id) {
 		HashMap<String, Object> m = new HashMap(10);
 		if (id == 10) {
+			long dob = Date.valueOf("2000-07-15").getTime();
+			long age = Instant.now().get
+			Instant now = Instant.now();
+			int age = now
+			m.put("age", age);
 			m.put("id", 10);
-			m.put("ref", "boh");
-			m.put("number", 17);
-			m.put("code", "codice");
-			m.put("name", "andrea");
-			m.put("nationality", "ita");
+			m.put("forename", "Andrea");
+			m.put("surname", "Crocco");
+			m.put("nationality", "IT");
 		} else {
 			m.put("error", "Id inesistente");
 		}
 		return m;
+	}
+
+	@RequestMapping("drivers")
+	public String[] drivers() {
+		String[] arr = new String[10];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = "mona";
+		}
+		return arr;
 	}
 }
