26ced72d4bb5143fcd7317e58966931b8165af60
Nicolò Tagliaferro <nico.taglia26@gmail.com>
2019-05-06 08:50:18 +0200

    Aggiunto /get/driver
    Rimossi i mapping non utilizzati

--- InitializrSpringbootProject/src/main/java/it/prepattag/F1/GetController.java
+++ InitializrSpringbootProject/src/main/java/it/prepattag/F1/GetController.java
@@ -1,18 +1,26 @@
 package it.prepattag.F1;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/get")
 public class GetController {
-	@RequestMapping("/nome")
-	public String nome() {
-		return Variables.nome;
-	}
-
-	@RequestMapping("/cognome")
-	public String cognome() {
-		return Variables.cognome;
+	@RequestMapping(value = "driver", params = "id", method = RequestMethod.GET)
+	public Map driver(@RequestParam("id") int id) {
+		HashMap<String, Object> m = new HashMap(10);
+		if (id == 10) {
+			m.put("id", 10);
+			m.put("ref", "boh");
+			m.put("number", 17);
+			m.put("code", "codice");
+		} else {
+			m.put("error", "Id inesistente");
+		}
+		return m;
 	}
 }
