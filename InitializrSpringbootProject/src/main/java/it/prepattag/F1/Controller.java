7cd7f161f5d53b2c917873ef197f65ae4966d682
Nicolò Tagliaferro <nico.taglia26@gmail.com>
2019-05-09 21:28:59 +0200

    Aggiunto supporto a JDBC

--- InitializrSpringbootProject/src/main/java/it/prepattag/F1/Controller.java
+++ InitializrSpringbootProject/src/main/java/it/prepattag/F1/Controller.java
@@ -1,11 +1,13 @@
 package it.prepattag.F1;
 
+import java.io.File;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller implements ErrorController {
+
     private static final String PATH = "/error";
 
     @RequestMapping(PATH)
